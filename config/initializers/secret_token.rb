e4d536573788a922b78139d5d2472f95af55ac25
Terrawind <matthieu.novi@gmail.com>
2014-01-12 05:33:13 -0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Myapp::Application.config.secret_key_base = '4964ad6963f9b367bea79d7a3ac45742d6a820e96e6ef9a5cc346ab70a0a8bfc0c07f437b914f23a94d04ada865ec57d209b7f0fa04d42097c5b14ffe0ddab24'
